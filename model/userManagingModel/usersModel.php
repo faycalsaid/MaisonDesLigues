ac3ee2abf31a6793e54bfd2c197d3fc451d400e7
Faycal <faycalhaboo@gmail.com>
2018-11-30 10:50:01 +0100

    Added Editing Users MDB Button Icons

--- model/userManagingModel/usersModel.php
+++ model/userManagingModel/usersModel.php
@@ -5,7 +5,7 @@ require "model/bddModel.php";
     function getAllUsers()
     {
         $db = dbConnect();
-        $res = $db->query("SELECT level,name,email FROM `mrbs_users`");
+        $res = $db->query("SELECT level,name,email FROM `mrbs_users` ORDER BY level DESC");
         $res = $res->fetchAll();
         return $res;
     }
--- vue/userManaging/usersView.php
+++ vue/userManaging/usersView.php
@@ -5,17 +5,32 @@
         <th scope="col">Nom</th>
         <th scope="col">Status</th>
         <th scope="col">Email</th>
+        <th scope="col"></th>
       </tr>
   </thead>
     
   <tbody>
     <?php 
-      foreach($users as $user):?>
+ 
+      foreach($users as $user):
+        if($user['level'] == 2)
+        {
+          $level = "Admin";
+        }
+        else if ($user['level'] == 1)
+        {
+          $level = "User";
+        }
+        else if($user['level'] == 0)
+        {
+          $level = "Null";
+        }?>
       
       <tr>
         <td><?= $user['name'];?></td>
-        <td><?= $user['level'];?></td>
+        <td><?= $level?></td>
         <td><?= $user['email'];?></td>
+        <td><button class="btn btn-default">Modifier <i class="fa fa-magic ml-1"></i></button></td>
       </tr>
     <?php endforeach ?>
   </tbody>
