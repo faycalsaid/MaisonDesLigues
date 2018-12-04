dc31d03a1daee3a64126ffd242693b8d069202b2
SharrlY <45202414+SharrlY@users.noreply.github.com>
2018-12-04 13:53:11 +0100

    Mahfoud second changes

--- vue/Salles/modify.php
+++ vue/Salles/modify.php
@@ -4,24 +4,24 @@
     <p class="h4 mb-4">Modification de la salle</p>
 
     <!-- Name -->
-    <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name">
+    <input type="text" id="Name" class="form-control mb-4" placeholder="Name">
 
     <!-- Email -->
-    <input type="email" id="defaultContactFormEmail" class="form-control mb-4" placeholder="E-mail">
+    <input type="email" id="Email" class="form-control mb-4" placeholder="E-mail">
 
     <!-- Capacity -->
-    <input type="text" id="defaultContactFormCapacity" class="form-control mb-4" placeholde="Capacité">
+    <input type="text" id="Capacity" class="form-control mb-4" placeholde="Capacité">
 
     
 
     <!-- Message -->
     <div class="form-group">
-        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Description"></textarea>
+        <textarea class="form-control rounded-0" id="Description" rows="3" placeholder="Description"></textarea>
     </div>
 
    
 
     <!-- Send button -->
-    <button class="btn btn-info btn-block" type="submit">Send</button>
+    <button class="btn btn-info btn-block"id="Valider" type="submit">Send</button>
 
 </form>
