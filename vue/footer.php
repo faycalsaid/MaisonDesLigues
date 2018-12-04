e14e8dd123a6cf2f304b65fc07b5235b5cdce2a3
feycce <45202367+feycce@users.noreply.github.com>
2018-12-04 13:46:30 +0100

    Fixing link conflicts

--- vue/footer.php
+++ vue/footer.php
@@ -1,11 +1,11 @@
   <!-- SCRIPTS -->
   <!-- JQuery -->
-  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="public/js/jquery-3.3.1.min.js"></script>
   <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="public/js/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="public/js/bootstrap.min.js"></script>
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script type="text/javascript" src="public/js/mdb.min.js"></script>
 </body>
 </html>
