da16704599ef24e568214ceaabc1273655fdfc3b
SharrlY <45202414+SharrlY@users.noreply.github.com>
2019-03-14 13:36:10 +0100

    (salle) add btn

--- model/salleManagingModel/salleModel.php
+++ model/salleManagingModel/salleModel.php
@@ -2,32 +2,32 @@
 
 
 
-    Function getSalleInfo()
+    function getSalleInfo()
     {
         $db = dbConnect();
-        $req = $db->query("SELECT room_name from mrbs_room where area_id = 1");
+        $req = $db->query("SELECT room_name, area_id,description,capacity,room_admin_email from mrbs_room where area_id = 1");
         $req = $req->fetchAll();
         return $req;
     }
 
-    Function getSalleReunion()
+    function getSalleReunion()
     {
         $db = dbConnect();
-        $req = $db->query("SELECT room_name from mrbs_room where area_id = 2");
+        $req = $db->query("SELECT room_name, area_id,description,capacity,room_admin_email from mrbs_room where area_id = 2");
         $req = $req->fetchAll();
         return $req;
     }
 
-    Function getSalleReception()
+    function getSalleReception()
     {
         $db = dbConnect();
-        $req = $db->query("SELECT room_name from mrbs_room where area_id = 3");
+        $req = $db->query("SELECT room_name, area_id,description,capacity,room_admin_email from mrbs_room where area_id = 3");
         $req = $req->fetchAll();
         return $req;
     }
 
     
-
+   
 
 
 
--- public/js/sallesEditing.js
+++ public/js/sallesEditing.js
@@ -2,7 +2,9 @@
 
  function editInfo(){
 $("#select_info").change(function(){ //Listener for select onChange to enable edit buton
-    $("#btnEditInfo").attr('disabled',false);
+	$("#btnEditInfo").attr('disabled',false);
+	
+
 });
  }
 
@@ -18,6 +20,29 @@ $("#select_reception").change(function(){ //Listener for select onChange to enab
 	$("#btnEditReception").attr('disabled',false);
 	});
 	 }
-	 
+
+
+	 function updateSalle()
+{
+    var area, description ,capacity , email;
+    
+    area = $('#formTypenNew').val();
+    escription = $('#inputName').val();
+    capacity = $('#inputPwdNewUser').val();
+    email = $('#inputEmailNew').val();
+    var targetFunction = 'insertUser';
+
+    $.ajax({
+        method: "POST",
+        context: document.body,
+        url: "model/userManagingModel/usersModel.php",
+        data: {targFunc: targetFunction , level: level, name: name, pwd : pwd, email : email },
+        dataType : "json"
+      })
+        .done(function( msg ) {
+            var obj = $.parseJSON(msg);
+            alert( "Data Saved: " + obj );
+        });
+}
 
 
